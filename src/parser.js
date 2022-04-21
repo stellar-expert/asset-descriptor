09ef1b56e9f7c33c078ffc171d0c1a9c19c8f025
orbitlens <orbit.lens@gmail.com>
2022-04-21 12:20:34 +0300

    Change asset parsing logic to properly handle empty object case

--- src/parser.js
+++ src/parser.js
@@ -2,7 +2,7 @@ import {AssetDescriptor, LiquidityPoolDescriptor} from './asset-descriptor'
 
 /**
  * Parse asset from Horizon API response
- * @param obj {Object} - Object to parse data from
+ * @param obj {Object} - Object that contains asset properties
  * @param prefix {String} - Optional field names prefix
  * @returns {AssetDescriptor}
  */
@@ -12,11 +12,11 @@ export function parseAssetFromObject(obj, prefix = '') {
         return new LiquidityPoolDescriptor(obj.liquidity_pool_id)
     if (obj[keyPrefix])  //new format
         return new AssetDescriptor(obj[keyPrefix])
-    const type = obj[keyPrefix + '_type']
-    if (!type)
-        throw new TypeError(`Invalid asset descriptor: ${JSON.stringify(obj)}. Prefix: ${prefix}`)
-    if (type === 'native')
+    const type = obj[keyPrefix + '_type'],
+        code = obj[keyPrefix + '_code'],
+        issuer = obj[keyPrefix + '_issuer']
+    if (type === 'native' || !type && !issuer)
         return AssetDescriptor.native
-    return new AssetDescriptor(obj[keyPrefix + '_code'], obj[keyPrefix + '_issuer'], obj[keyPrefix + '_type'])
+    return new AssetDescriptor(code, issuer, type)
 }
 
